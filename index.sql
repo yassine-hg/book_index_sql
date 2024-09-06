a7bd28b560073b1e5368f9e352d033252c1d6952
yassine-hg <160339862+yassine-hg@users.noreply.github.com>
2024-09-06 16:18:16 +0100

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,1 +1,9 @@
+CREATE INDEX  customers_state_name_email_address_idx ON customers(state_name, email_address);
 
+EXPLAIN ANALYZE SELECT state_name, email_address FROM customers WHERE state_name = 'California' OR state_name = 'Ohio'
+ORDER BY state_name DESC, email_address;
+
+CREATE INDEX customers_state_name_email_address_ordered_idx ON customers (state_name DESC, email_address);
+
+EXPLAIN ANALYZE SELECT state_name, email_address FROM customers WHERE state_name = 'California' OR state_name = 'Ohio'
+ORDER BY state_name DESC, email_address;
